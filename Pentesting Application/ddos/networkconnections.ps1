7df3d97e9d9b3371dd3b29d3da3defaf66103540
unknown <djrk01>
2025-04-10 10:12:41 -0400

    Initial commit

---
+++ Pentesting Application/ddos/networkconnections.ps1
@@ -0,0 +1,26 @@
+# Get all network connections
+$connections = Get-NetTCPConnection -State Established
+
+# Create a file to store the output
+$outputFile = "C:\NetworkConnections.txt"
+"Network Connections:" | Out-File -FilePath $outputFile -Encoding utf8
+
+# Loop through the connections and extract the relevant information
+foreach ($connection in $connections) {
+  $localAddress = $connection.LocalAddress
+  $localPort = $connection.LocalPort
+  $remoteAddress = $connection.RemoteAddress
+  $remotePort = $connection.RemotePort
+  $processId = $connection.OwningProcess
+  $processName = (Get-Process -Id $processId).ProcessName
+
+  # Output the connection information to the file
+  "Connection found:" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+  "  Local Address: $localAddress" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+  "  Local Port: $localPort" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+  "  Remote Address: $remoteAddress" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+  "  Remote Port: $remotePort" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+  "  Process ID: $processId" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+  "  Process Name: $processName" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+  "" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+}
